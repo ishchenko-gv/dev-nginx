e864ae99fdf505494554af9dbd4a24df1c43ee6a
ubuntu <ubuntu@podcutter-dev.com>
2025-04-18 17:31:25 +0000

    add auth

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 docker run \
+	-d \
 	-it \
 	--rm \
-	-d \
 	--name web \
 	--network="host" \
 	-v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
+	-v /etc/apache2/.htpasswd:/etc/nginx/.htpasswd \
 	-v /home/ubuntu/static:/var/www/data:ro \
 	nginx
